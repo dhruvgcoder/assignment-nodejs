83641a5e63534b10bdfa99829368d010ab609dfe
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-05-30 20:21:28 +0530

    MySQL db schema file added

---
+++ schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY,
+    username VARCHAR(100),
+    full_name VARCHAR(100),
+    location VARCHAR(100),
+    total_repos INT,
+    followers INT
+);
+
+CREATE TABLE IF NOT EXISTS repos (
+    id INT PRIMARY KEY,
+    owner_id INT,
+    repo_name VARCHAR(200),
+    description VARCHAR(500),
+    url VARCHAR(500),
+    language VARCHAR(100),
+    created_at TIMESTAMP,
+    stars INT,
+    size_in_kb INT,
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+);
