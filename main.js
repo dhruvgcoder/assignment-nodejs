f5eccd56ef60a82d263b59b61ce36c38de9765bb
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-05-30 19:57:07 +0530

    controllers imported in entry file

--- main.js
+++ main.js
@@ -1,39 +1,24 @@
 const express = require("express")
-const mysql = require("mysql2")
+const dotenv = require("dotenv")
+
+const dbConnection  = require("./db/connection")
+const data = require("./controllers/getData.controller")
+const repos = require("./controllers/getRepos.controller")
 
 const app = express()
-
 app.use(express.json())
 dotenv.config()
 
 
-app.get("/data/:username", async (req, res) => {
-    const username = req.params.username
-    
-    async function getData() {
-        const data = await fetch(`https://api.github.com/users/${username}`)
-        return data.json()
-    }
-    const data = await getData()
-    res.json({
-        data: data
+app.use("/user", data)
+app.use("/user", repos)
+
+async function startServer(){
+    await dbConnection()
+    app.listen(3001,()=>{
+        console.log("Server started running on PORT 3001")
     })
-})
+}
 
+startServer()
 
-const connection = mysql.createConnection(process.env.DB_URL)
-
-connection.connect((err)=>{
-    if(err){
-        console.log({
-            msg : "Error while connecting to MySQL",
-            err : err
-        })
-        return
-    }
-    console.log("Connected to MySQL successfully")
-
-    app.listen(3001, () => {
-        console.log("Server is running on PORT 3001")
-    })
-})
