9688289792090d27ed5b00510758702f146a7074
Dhruv Gupta <dhruvgcoder@gmail.com>
2026-05-30 20:45:41 +0530

    dynamic port added and api health status route added

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ const repos = require("./controllers/getRepos.controller")
 const app = express()
 app.use(express.json())
 
-
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        status : "ok",
+        msg : "API is running"
+    })
+})
 app.use("/user", data)
 app.use("/user", repos)
 
+const PORT = process.env.PORT || 3001
+
 async function startServer(){
     await dbConnection()
-    app.listen(3001,()=>{
-        console.log("Server started running on PORT 3001")
+    app.listen(PORT,()=>{
+        console.log(`Server started running on ${PORT}`)
     })
 }
 
